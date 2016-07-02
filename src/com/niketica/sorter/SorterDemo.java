c72a7f2914f90ff3aa785d4d0470d67d28b3d086
Alexander <Alexander@172.16.9.184>
2016-07-02 20:22:18 +1000

    First JavaDoc.

--- src/com/niketica/sorter/SorterDemo.java
+++ src/com/niketica/sorter/SorterDemo.java
@@ -1,44 +1,45 @@
 package com.niketica.sorter;
 
+/**
+ * SorterDemo is a basic class that showcases the different sorting methods.
+ * @author Alexander
+ */
 public class SorterDemo {
-	private NumberListGenerator NLG = new NumberListGenerator(20, 5, 100);
-	private int[] numberList;
-	private int[] sortedList;
-	private Sorter s;
 	
 	public SorterDemo(){
-		bubbleSort();
-		System.out.println();
-		mergeSort();
-		System.out.println();
-		insertionSort();
+		startSort(SorterType.BUBBLE_SORT);
+		startSort(SorterType.MERGE_SORT);
+		startSort(SorterType.INSERTION_SORT);
 	}
-	
-	private void bubbleSort(){
-		numberList = NLG.generateNumberList();
-		s = SorterFactory.getSorter(SorterType.BUBBLE_SORT);
+
+	/**
+	 * Start sorting a random number list using a given sorter type.
+	 * @param type The sorting type used to sort the numbers.
+	 */
+	private void startSort(SorterType type){
+		NumberListGenerator NLG = new NumberListGenerator(20, 5, 100);
+		int[] numberList = NLG.generateNumberList();
+		int[] sortedList;
+		Sorter s;
 		
-		System.out.println("Bubble Sort Example");
+		switch(type){
+		case MERGE_SORT:
+			s = SorterFactory.getSorter(SorterType.MERGE_SORT);
+			System.out.println("Merge Sort Example");
+			break;
+		case BUBBLE_SORT:
+			s = SorterFactory.getSorter(SorterType.BUBBLE_SORT);
+			System.out.println("Bubble Sort Example");
+			break;
+		case INSERTION_SORT:
+		default:
+			s = SorterFactory.getSorter(SorterType.INSERTION_SORT);
+			System.out.println("Insertion Sort Example");
+		}
+		
 		NumberListGenerator.printNumberList(numberList);
 		sortedList = s.sortList(numberList);
 		NumberListGenerator.printNumberList(sortedList);
-	}
-	
-	private void mergeSort(){
-		numberList = NLG.generateNumberList();
-		s = SorterFactory.getSorter(SorterType.MERGE_SORT);
-		System.out.println("Merge Sort Example");
-		NumberListGenerator.printNumberList(numberList);
-		sortedList = s.sortList(numberList);
-		NumberListGenerator.printNumberList(sortedList);
-	}
-	
-	private void insertionSort(){
-		numberList = NLG.generateNumberList();
-		s = SorterFactory.getSorter(SorterType.INSERTION_SORT);
-		System.out.println("Insertion Sort Example");
-		NumberListGenerator.printNumberList(numberList);
-		sortedList = s.sortList(numberList);
-		NumberListGenerator.printNumberList(sortedList);		
+		System.out.println();
 	}
 }
